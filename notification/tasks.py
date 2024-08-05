eeb675b3e0a8e895e764874152078205dc6c548b
hyunnjjung <hyunnjjung@gmail.com>
2024-08-05 13:50:43 +0900

    add : test 코드 추가

--- notification/tasks.py
+++ notification/tasks.py
@@ -1,8 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.utils import timezone
+from missions.models import DailyInfo
 from .models import Notification
 from django.conf import settings
 from django.contrib.auth import get_user_model
+import datetime
 
 User = get_user_model()
 
@@ -10,9 +12,17 @@ def send_daily_mission_notifications():
     now = timezone.now()
     users = User.objects.all()
     for user in users:
-        message = "오늘의 미션을 완료했는지 확인해 보세요!"
-        url_text = "미션 확인하러 가기"
-        Notification.objects.create(user=user, message=message, url_text = url_text)
+        try:
+            daily_info = DailyInfo.objects.get(user=user, date=datetime.date.today)
+            if not daily_info.all_completed:
+                message = "오늘의 미션을 완료했는지 확인해 보세요!"
+                url_text = "미션 확인하러 가기"
+                Notification.objects.create(user=user, message=message, url_text=url_text)
+        except DailyInfo.DoesNotExist:
+            # If there is no DailyInfo record for today, we assume the missions are not completed.
+            message = "오늘의 미션을 완료했는지 확인해 보세요!"
+            url_text = "미션 확인하러 가기"
+            Notification.objects.create(user=user, message=message, url_text=url_text)
 
 def send_health_tip_notifications():
     now = timezone.now()
@@ -34,4 +44,4 @@ scheduler = BackgroundScheduler()
 
 scheduler.add_job(send_daily_mission_notifications, 'interval',  days=1, start_date='2024-08-04 00:21:00', id='daily_mission')
 scheduler.add_job(send_health_tip_notifications, 'interval', hours=12, start_date='2024-08-04 00:08:00', id='health_tip')
-scheduler.add_job(send_acupressure_point_notifications, 'interval', hours=12, start_date='2024-08-04 00:08:00', id='acupressure_point')
+scheduler.add_job(send_acupressure_point_notifications, 'interval', minutes=1, start_date='2024-08-04 00:08:00', id='acupressure_point')
